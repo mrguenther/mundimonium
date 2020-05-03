3e24472df7eb41fd7ad37c5b7ce56db82cb49088
Matthias Guenther <matthias.r.guenther@gmail.com>
2020-05-03 13:42:09 -0700

    Flesh out tessellation vertex/face adjacency logic
    Automate more adjacency logic, fix some of the existing logic, and add
    more thorough example output as the `main` function of tessellation.py.

--- mundimonium/layers/coordinates/tessellation.py
+++ mundimonium/layers/coordinates/tessellation.py
@@ -44,17 +44,30 @@ class TessellationVertex(HashByIndex):
 		self._adjacent_faces = list()
 
 	def add_adjacent_face(self, face: "TessellationFace") -> None:
-		if face not in self._adjacent_faces:
-			self._adjacent_faces.append(face)
+		if face in self._adjacent_faces:
+			return
 
-	def is_adjacent_to(self, face: "TessellationFace") -> bool:
+		self._adjacent_faces.append(face)
+
+		for other_face in self._adjacent_faces:
+			if other_face is not face:
+				other_face.recalculate_adjacency_to(face)
+
+	def is_adjacent_to_face(self, face: "TessellationFace") -> bool:
 		return face in self._adjacent_faces
 
-	def is_adjacent_to(self, vertex: "TessellationVertex") -> bool:
+	def is_adjacent_to_vertex(self, vertex: "TessellationVertex") -> bool:
 		for face in self._adjacent_faces:
-			if face.is_adjacent_to(vertex):
+			if face.is_adjacent_to_vertex(vertex):
 				return vertex is not self
 		return False
+
+	def _is_adjacent_to_selector(self, arg_type: type):
+		return {TessellationFace: self.is_adjacent_to_face,
+				TessellationVertex: self.is_adjacent_to_vertex}[arg_type]
+
+	def is_adjacent_to(self, other):
+		return self._is_adjacent_to_selector(type(other))(other)
 
 	@property
 	def tessellation_type(self):
@@ -122,6 +135,19 @@ class TessellationFace(HashByIndex, IsometricGrid):
 		self._centroid = None
 		self.recalculate_centroid()
 
+	def is_adjacent_to_face(self, face: "TessellationFace") -> bool:
+		return face in self._adjacent_faces
+
+	def is_adjacent_to_vertex(self, vertex: "TessellationVertex") -> bool:
+		return vertex in self._adjacent_vertices
+
+	def _is_adjacent_to_selector(self, arg_type: type):
+		return {TessellationFace: self.is_adjacent_to_face,
+				TessellationVertex: self.is_adjacent_to_vertex}[arg_type]
+
+	def is_adjacent_to(self, other):
+		return self._is_adjacent_to_selector(type(other))(other)
+
 	def vertex_at(self, opposite_edge: "IsometricDirection"
 			) -> "TessellationVertex":
 		return self._adjacent_vertices[opposite_edge.value]
@@ -137,19 +163,41 @@ class TessellationFace(HashByIndex, IsometricGrid):
 					self._adjacent_vertices.index(adjacent_vertex))
 		except ValueError:
 			raise NotAdjacentException(
-					"The provided vertex is not adjacent to this face.")
+					"The provided vertex is not adjacent to this face."
+					) from None
 
-	def direction_from_face(self, adjacent_face: "TessellationFace"
+	def direction_opposite_face(self, adjacent_face: "TessellationFace"
 			) -> "IsometricDirection":
 		try:
 			return IsometricDirection(self._adjacent_faces.index(adjacent_face))
 		except ValueError:
-			raise NotAdjacentException("The provided faces are not adjacent.")
+			raise NotAdjacentException(
+					"The provided faces are not adjacent.") from None
 
-	def recalculate_centroid(self):
+	def recalculate_centroid(self) -> None:
 		self._centroid = tuple(
-			sum([getattr(vert, axis) for vert in self._adjacent_vertices]) /
+			sum([getattr(v, axis) for v in self._adjacent_vertices]) /
 			len(self._adjacent_vertices) for axis in "xyz")
+
+	def recalculate_adjacency_to(
+			self, other_face: "TessellationFace", call_bilaterally: bool = True
+			) -> None:
+		shared_vertices = [
+				v.is_adjacent_to_face(other_face) \
+				for v in self._adjacent_vertices]
+
+		adjacent = (sum(shared_vertices) == 2)
+
+		for direction in IsometricDirection:
+			if adjacent and not shared_vertices[direction.value]:
+				# The `not` is because the face associated with `direction` is
+				# opposite the vertex associated with `direction`.
+				self._adjacent_faces[direction.value] = other_face
+			elif self._adjacent_faces[direction.value] is other_face:
+				self._adjacent_faces[direction.value] = None
+
+		if (adjacent and call_bilaterally):
+			other_face.recalculate_adjacency_to(self, call_bilaterally=False)
 
 	@property
 	def side_length(self):
@@ -178,20 +226,72 @@ class TessellationFace(HashByIndex, IsometricGrid):
 
 if __name__ == '__main__':
 	apothem = math.sqrt(3)/6
+
 	vert_b = TessellationVertex([0, 2*apothem, 0])
 	vert_s = TessellationVertex([-1/2, -apothem, 0])
 	vert_d = TessellationVertex([1/2, -apothem, 0])
+
 	grid = TessellationFace(vert_b, vert_s, vert_d)
-	print("Centroid:", grid.centroid)
+
+	vert_b_prime = TessellationVertex([
+			0, -(math.sqrt(3)*3/2*apothem + 1), -3/2*apothem])
+	vert_s_prime = vert_d
+	vert_d_prime = vert_s
+
+	grid_prime = TessellationFace(vert_b_prime, vert_s_prime, vert_d_prime)
+
 	a = IsometricPoint.center(grid)
 	b = IsometricPoint(grid, apothem + 0.2, apothem + 0.2)
 	c = IsometricPoint(grid, apothem + 0.2, apothem - 0.2)
+
+	adjacency_string = lambda node: " ".join(
+			[str(node.is_adjacent_to(other)).ljust(5) for other in [
+					grid, vert_b, vert_s, vert_d,
+					grid_prime, vert_b_prime, vert_s_prime, vert_d_prime]])
+
+	print()
+	print("Adjacency matrix:")
+	print("      | grid   b     s     d    grid'  b'    s'    d'  ")
+	print("------+------------------------------------------------")
+	print("grid  |", adjacency_string(grid))
+	print("b     |", adjacency_string(vert_b))
+	print("s     |", adjacency_string(vert_s))
+	print("d     |", adjacency_string(vert_d))
+	print("grid' |", adjacency_string(grid_prime))
+	print("b'    |", adjacency_string(vert_b_prime))
+	print("s'    |", adjacency_string(vert_s_prime))
+	print("d'    |", adjacency_string(vert_d_prime))
+	print()
+	print("Centroid of grid:      ", grid.centroid)
+	print("Centroid of grid_prime:", grid_prime.centroid)
+	print()
+	print("Local directions facing toward adjacent vertices:")
+	print(f"grid  -> b:  {grid.direction_toward_vertex(vert_b)}")
+	print(f"grid  -> s:  {grid.direction_toward_vertex(vert_s)}")
+	print(f"grid  -> d:  {grid.direction_toward_vertex(vert_d)}")
+	print(f"grid' -> b': {grid_prime.direction_toward_vertex(vert_b_prime)}")
+	print(f"grid' -> s': {grid_prime.direction_toward_vertex(vert_s_prime)}")
+	print(f"grid' -> d': {grid_prime.direction_toward_vertex(vert_d_prime)}")
+	print()
+	print("Local directions facing toward adjacent faces:")
+	print(f"grid -> grid_prime: -{grid.direction_opposite_face(grid_prime)}")
+	print(f"grid_prime -> grid: -{grid_prime.direction_opposite_face(grid)}")
+	print()
+	print("Points (b,s,d) within grid:")
 	print("a:", a)
 	print("b:", b)
-	print("c:", b)
-	print(f"(a - b): |{a - b}| = {(a - b).length} = {a.distance_from(b)}")
-	print(f"(b - a): |{b - a}| = {(b - a).length} = {b.distance_from(a)}")
-	print(f"(a - c): |{a - c}| = {(a - c).length} = {a.distance_from(c)}")
-	print(f"(c - a): |{c - a}| = {(c - a).length} = {c.distance_from(a)}")
-	print(f"(b - c): |{b - c}| = {(b - c).length} = {b.distance_from(c)}")
-	print(f"(c - b): |{c - b}| = {(c - b).length} = {c.distance_from(b)}")
+	print("c:", c)
+	print()
+	print("Vectors <Δb,Δs,Δd> within grid:")
+	assert((a - b).length == a.distance_from(b))
+	assert((b - a).length == b.distance_from(a))
+	assert((a - c).length == a.distance_from(c))
+	assert((c - a).length == c.distance_from(a))
+	assert((b - c).length == b.distance_from(c))
+	assert((c - b).length == c.distance_from(b))
+	print(f"(a - b): |{a - b}| = {(a - b).length}")
+	print(f"(b - a): |{b - a}| = {(b - a).length}")
+	print(f"(a - c): |{a - c}| = {(a - c).length}")
+	print(f"(c - a): |{c - a}| = {(c - a).length}")
+	print(f"(b - c): |{b - c}| = {(b - c).length}")
+	print(f"(c - b): |{c - b}| = {(c - b).length}")
